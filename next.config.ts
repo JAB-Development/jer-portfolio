40c6b9a98344367bce1decf59e01d3a52edff6a8
Jerry-Bayoneta <jrbayonetajr@gmail.com>
2025-09-09 15:30:38 +0800

    update tsconfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: "build", // example
+  output: "export",
 };
 
 export default nextConfig;
